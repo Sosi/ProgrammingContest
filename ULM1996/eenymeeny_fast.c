a6f55428bf682309f8d8812a608c9a4f1d760d05
Sosi <sosisarah@gmail.com>
2013-05-27 23:07:44 +0800

    ULM1996
    http://poj.org/searchproblem?field=source&key=Ulm+Local+1996

---
+++ ULM1996/eenymeeny_fast.c
@@ -0,0 +1,35 @@
+/* University of Ulm Programming Contest 1996
+   Problem E: Eeny Meeny Moo
+   Fast algorithm by Falk Bartels */
+
+#include <stdio.h>
+
+int n,m;
+
+int suitable (int m)
+{
+    int cities,p;
+
+    cities = n-1;
+    p = (m-1)%cities;
+    while (cities>1)
+    {
+        if (p==0) return 0;
+        cities--;
+        p = (p+m-1)%cities;
+    }
+    return 1;
+}
+
+main()
+{
+
+    while (1)
+    {
+        scanf("%d",&n);
+        if (n==0) break;
+        for (m=2; !suitable(m); m++);
+        printf("%d\n",m);
+    }
+    return 0;
+}
