bef0657c162cdc15702ecf497bf44d701112fd39
XiaoleiLv <sosisarah@gmail.com>
2013-06-14 12:47:14 +0800

    TCO13 Round 1B

---
+++ SRM/TCO13 Round 1B/EllysReversals.cpp
@@ -0,0 +1,156 @@
+// BEGIN CUT HERE
+
+// END CUT HERE
+#line 5 "EllysReversals.cpp"
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cctype>
+#include <string>
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+ 
+// BEGIN CUT HERE
+//Begin Sosi TopCoder 
+//const double EPS=1e-11;
+//const double PI=acos(-1.0);
+//const short INF=32767,NF= -32768;
+//const int INF=2147483647,NF= -2147483648;
+//const long long INF=9223372036854775807,NF=-9223372036854775808;
+//const long double INF=99999999.99999999;
+ 
+//Numberic Functions
+//template<class T> T gcd(T a,T b){ if(a<0) return gcd(-a,b);if(b<0)return gcd(a,-b);return (b==0)?a:gcd(b,a%b);}
+//template <class T> T lcm(T a,T b){ return a*(b/gcd(a,b));}
+//template<class T> inline vector<pair<T,int> > factorize(T n)
+//   {vector<pair<T,int> > R;for (T i=2;n>1;){if (n%i==0){int C=0;for (;n%i==0;C++,n/=i);R.push_back(make_pair(i,C));}
+//   i++;if (i>n/i) i=n;}if (n>1) R.push_back(make_pair(n,1));return R;}
+//template<class T> inline bool isPrimeNumber(T n)
+//   {if(n<=1)return false;for (T i=2;i*i<=n;i++) if (n%i==0) return false;return true;}
+//template<class T> inline T eularFunction(T n)
+//   {vector<pair<T,int> > R=factorize(n);T r=n;for (int i=0;i<R.size();i++)r=r/R[i].first*(R[i].first-1);return r;}
+ 
+//Translator
+//template<class T> string toString(T n){ostringstream ost;ost<<n;ost.flush();return ost.str();}
+//int toInt(string s){int r=0;istringstream sin(s);sin>>r;return r;}//NOTES:toInt(
+//long long toInt64(string s){long long r=0;istringstream sin(s);sin>>r;return r;}
+//double toDouble(string s){double r=0;istringstream sin(s);sin>>r;return r;}//NOTES:toDouble(
+//template<class T> void stoa(string s,int &n,T A[]){n=0;istringstream sin(s);for(T v;sin>>v;A[n++]=v);}
+//template<class T> void atos(int n,T A[],string &s){ostringstream sout;for(int i=0;i<n;i++){if(i>0)sout<<' ';sout<<A[i];}s=sout.str();}
+//template<class T> void atov(int n,T A[],vector<T> &vi){vi.clear();for (int i=0;i<n;i++) vi.push_back(A[i]);}
+//template<class T> void vtoa(vector<T> vi,int &n,T A[]){n=vi.size();for (int i=0;i<n;i++)A[i]=vi[i];}
+//template<class T> void stov(string s,vector<T> &vi){vi.clear();istringstream sin(s);for(T v;sin>>v;vi.push_bakc(v));}
+//template<class T> void vtos(vector<T> vi,string &s){ostringstream sout;for (int i=0;i<vi.size();i++){if(i>0)sout<<' ';sout<<vi[i];}s=sout.str();}
+ 
+//Fraction
+//template<class T> struct Fraction{T a,b;Fraction(T a=0,T b=1);string toString();};
+//template<class T> Fraction<T>::Fraction(T a,T b){T d=gcd(a,b);a/=d;b/=d;if (b<0) a=-a,b=-b;this->a=a;this->b=b;}
+//template<class T> string Fraction<T>::toString(){ostringstream sout;sout<<a<<"/"<<b;return sout.str();}
+//template<class T> Fraction<T> operator+(Fraction<T> p,Fraction<T> q){return Fraction<T>(p.a*q.b+q.a*p.b,p.b*q.b);}
+//template<class T> Fraction<T> operator-(Fraction<T> p,Fraction<T> q){return Fraction<T>(p.a*q.b-q.a*p.b,p.b*q.b);}
+//template<class T> Fraction<T> operator*(Fraction<T> p,Fraction<T> q){return Fraction<T>(p.a*q.a,p.b*q.b);}
+//template<class T> Fraction<T> operator/(Fraction<T> p,Fraction<T> q){return Fraction<T>(p.a*q.b,p.b*q.a);}
+ 
+//STL
+//bool comp(T A,T B){return A<B?1:0; }
+//do{ } while(next_permutation(T.begin(), T.end()));
+//int dirx[]={1,0,-1,0};
+//int diry[]={0,-1,0,1}; //clockwise
+//int dirx[]={1, 1, 0,-1,-1,-1,0,1};
+//int diry[]={0,-1,-1,-1, 0, 1,1,1}; //clockwise
+//End Sosi TopCoder
+// END CUT HERE
+
+int i,j,k;
+#define REP(i,n) for((i)=0;(i)<(int)(n);(i)++)
+#define snuke(c,itr) for(__typeof((c).begin()) itr=(c).begin();itr!=(c).end();itr++)
+bool equal(string A, string B)
+{
+    if(A.size()!=B.size()) return 0;
+    if(A.size() %2 ==1 && A[A.size()-1] != B[B.size()-1]) return 0;
+    multiset<string> a;
+    multiset<string> b;
+    for(int i=0; i<A.size()/2*2; i+=2)
+    {
+        string tmp = "";
+        if(A[i] < A[i+1]) tmp+= A[i], tmp+= A[i+1];
+        else tmp+= A[i+1],tmp+= A[i];
+        a.insert(tmp);
+        tmp = "";
+        if(B[i] < B[i+1]) tmp+= B[i], tmp+= B[i+1];
+        else tmp+= B[i+1], tmp+= B[i];
+        b.insert(tmp);
+    }
+    return a == b;
+}
+class EllysReversals
+{
+        public:
+        int getMin(vector <string> words)
+        {
+            int vis[55];
+            memset(vis,0,sizeof(vis));
+            REP(k,words.size())
+            {
+                if(vis[k]) continue;
+                for(j=k+1;j<words.size(); j++)
+                {
+                    if(!vis[j] && equal(words[k], words[j]))
+                    {
+                        cout<<words[k]<< " "<<words[j]<<endl;
+                        vis[k] = 1;
+                        vis[j] = 1;
+                        break;
+                    }
+                }
+            }
+            int num = 0;
+            REP(i,words.size()) if(!vis[i]) num++;
+            return num;
+        }
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { string Arr0[] = {"esprit", "god", "redotopc", "odcpoter", "dog"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 3; verify_case(0, Arg1, getMin(Arg0)); }
+	void test_case_1() { string Arr0[] = {"no", "zaphod", "just", "very", "improbable"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 5; verify_case(1, Arg1, getMin(Arg0)); }
+	void test_case_2() { string Arr0[] = {"rats", "live", "stressed", "to", "act", "as", "star", "desserts", "of", "evil", "cat", "sa", "fo", "ot"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 0; verify_case(2, Arg1, getMin(Arg0)); }
+	void test_case_3() { string Arr0[] = {"topcoder", "redocpot", "doretopc", "cpotdoer", "harlemshake"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 1; verify_case(3, Arg1, getMin(Arg0)); }
+	void test_case_4() { string Arr0[] = {"iprlzgukfggzg", "bmhxvjbrtkbxy", "khapjiabbny", "nqlwgmcyvdikt",
+ "nxromtvtpug", "leealcapovm", "ushnxwjczczbmd", "bwhykzupcux",
+ "xrlboyuwlnsp", "bbjoketeheezfs", "dxfztrldomjqkv", "dkbktqdtgfujcut",
+ "zfybzyuxgpnt", "ffmsldrdftode", "vopuufksxd", "pqhbsiujwda",
+ "yhwbkzupcux", "hkbabnapjiy", "zqsqefrrzehtxn", "yovinyguyudmv"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 16; verify_case(4, Arg1, getMin(Arg0)); }
+
+// END CUT HERE
+
+};
+// BEGIN CUT HERE
+int main()
+{
+        EllysReversals ___test;
+        ___test.run_test(-1);
+        return 0;
+}
+// END CUT HERE
