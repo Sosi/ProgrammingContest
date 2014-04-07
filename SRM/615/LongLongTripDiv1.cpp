0d120cb64d8fcb1fecf39fa77887d2f7dfeb8d6e
Sosi <sosisarah@gmail.com>
2014-04-07 17:43:08 +0800

    SRM 615

--- SRM/615/LongLongTripDiv1.cpp
+++ SRM/615/LongLongTripDiv1.cpp
@@ -34,7 +34,7 @@ int i,j,k;
 //const int INF=2147483647,NF= -2147483648;
 //const long long INF=9223372036854775807,NF=-9223372036854775808;
 //const long double INF=99999999.99999999;
- 
+
 //Numberic Functions
 //template<class T> T gcd(T a,T b){ if(a<0) return gcd(-a,b);if(b<0)return gcd(a,-b);return (b==0)?a:gcd(b,a%b);}
 //template <class T> T lcm(T a,T b){ return a*(b/gcd(a,b));}
@@ -45,7 +45,7 @@ int i,j,k;
 //   {if(n<=1)return false;for (T i=2;i*i<=n;i++) if (n%i==0) return false;return true;}
 //template<class T> inline T eularFunction(T n)
 //   {vector<pair<T,int> > R=factorize(n);T r=n;for (int i=0;i<R.size();i++)r=r/R[i].first*(R[i].first-1);return r;}
- 
+
 //Translator
 //template<class T> string toString(T n){ostringstream ost;ost<<n;ost.flush();return ost.str();}
 //int toInt(string s){int r=0;istringstream sin(s);sin>>r;return r;}//NOTES:toInt(
@@ -58,7 +58,7 @@ int i,j,k;
 
 //template<class T> void stov(string s,vector<T> &vi){vi.clear();istringstream sin(s);for(T v;sin>>v;vi.push_bakc(v));}
 //template<class T> void vtos(vector<T> vi,string &s){ostringstream sout;for (int i=0;i<vi.size();i++){if(i>0)sout<<' ';sout<<vi[i];}s=sout.str();}
- 
+
 //Fraction
 //template<class T> struct Fraction{T a,b;Fraction(T a=0,T b=1);string toString();};
 //template<class T> Fraction<T>::Fraction(T a,T b){T d=gcd(a,b);a/=d;b/=d;if (b<0) a=-a,b=-b;this->a=a;this->b=b;}
@@ -67,7 +67,7 @@ int i,j,k;
 //template<class T> Fraction<T> operator-(Fraction<T> p,Fraction<T> q){return Fraction<T>(p.a*q.b-q.a*p.b,p.b*q.b);}
 //template<class T> Fraction<T> operator*(Fraction<T> p,Fraction<T> q){return Fraction<T>(p.a*q.a,p.b*q.b);}
 //template<class T> Fraction<T> operator/(Fraction<T> p,Fraction<T> q){return Fraction<T>(p.a*q.b,p.b*q.a);}
- 
+
 //STL
 //bool comp(T A,T B){return A<B?1:0; }
 //do{ } while(next_permutation(T.begin(), T.end()));
@@ -78,18 +78,80 @@ int i,j,k;
 //End Sosi TopCoder
 // END CUT HERE
 
+
+
+#define node  pair<int, int> 
+#define MP make_pair
 class LongLongTripDiv1
 {
 public:
-string isAble(int N, vector <int> A, vector <int> B, vector <int> D, long long T)
-{
-//$CARETPOSITION$
-}
 
-// BEGIN CUT HERE
-	public:
+	static const long long  INF =  0x7F7F7F7F7F7F7F7F;
+	long long dist[20000][50];
+
+	string isAble(int N, vector<int> A, vector<int> B, vector<int> D, long long T)
+	{
+		// pick d, although not important for worst-case complexity, we can
+		// pick the minimum possible d.
+		int d = 10001;
+		for (int i = 0; i < A.size(); i++) {
+			if ( (A[i] == 0) || (B[i] == 0) ) {
+				d = std::min(d, D[i]);
+			}
+		}
+		
+		// a std::set acts as the priority queue, c++ specific,
+		// the advantage over priority queues is that
+		// it allows deletion in O(log(n)) time.
+		set< pair<long,node> > Q;
+		// Dijkstra: initialize distance array to infinite. 
+		for(auto i= 0; i<20000; i++) for(auto j = 0; j<50; j++) dist[i][j] = INF;
+		//memset(dist, BYTE_INF, sizeof(dist));
+		// now insert (0,0) to the queue, with distance 0, also mark the distance.
+		Q.insert(  MP(0ll, MP(0, 0)) );
+		dist[0][0] = 0;
+		// Dijkstra: While there are elements in the queue:
+		while ( ! Q.empty() ) {
+			// save the element
+			auto curr = Q.begin()->second;
+			long x = dist[curr.first][curr.second];             
+			int u = curr.second;
+			// remove element from queue:
+			Q.erase(Q.begin());
+			// try the edges:
+			for (int i = 0; i < B.size(); i++) {
+				int v = -1;
+				if (A[i] == u) {
+					v = B[i];
+				}
+				if (B[i] == u) {
+					v = A[i];
+				}
+				// there is an edge of length D[i] connecting u to v.
+				if (v != -1) {
+					int m = (curr.first + D[i]) % (2*d);
+					long nx = x + D[i];
+					// a new possible distance from (0,0) to (m,v) : nx
+					if (nx < dist[m][v]) {
+						// update queue and distance table:
+						auto q = Q.find( MP(dist[m][v], MP(m,v)) );
+						if (q != Q.end()) {
+							Q.erase(q);
+						}
+						dist[m][v] = nx;
+						Q.insert( MP(nx, MP(m,v) ) );
+					}
+				}
+			}
+		}
+		//if the minimum distance exists and is valid...
+		return (dist[ T%(2*d) ][N -1] <= T) ? "Possible": "Impossible";
+	}
+
+	// BEGIN CUT HERE
+public:
 	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); if ((Case == -1) || (Case == 5)) test_case_5(); }
-	private:
+private:
 	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
 	void verify_case(int Case, const string &Expected, const string &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
 	void test_case_0() { int Arg0 = 3; int Arr1[] = {0,0,1}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {2,1,2}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); int Arr3[] = {7,6,5}; vector <int> Arg3(Arr3, Arr3 + (sizeof(Arr3) / sizeof(Arr3[0]))); long long Arg4 = 11LL; string Arg5 = "Possible"; verify_case(0, Arg5, isAble(Arg0, Arg1, Arg2, Arg3, Arg4)); }
@@ -99,15 +161,14 @@ string isAble(int N, vector <int> A, vector <int> B, vector <int> D, long long T
 	void test_case_4() { int Arg0 = 4; int Arr1[] = {0,0,1}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {2,1,2}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); int Arr3[] = {10,10,10}; vector <int> Arg3(Arr3, Arr3 + (sizeof(Arr3) / sizeof(Arr3[0]))); long long Arg4 = 1000LL; string Arg5 = "Impossible"; verify_case(4, Arg5, isAble(Arg0, Arg1, Arg2, Arg3, Arg4)); }
 	void test_case_5() { int Arg0 = 9; int Arr1[] = {4,8,5,8,3,6,2,6,7,6,6}; vector <int> Arg1(Arr1, Arr1 + (sizeof(Arr1) / sizeof(Arr1[0]))); int Arr2[] = {2,7,1,5,1,3,1,1,5,4,2}; vector <int> Arg2(Arr2, Arr2 + (sizeof(Arr2) / sizeof(Arr2[0]))); int Arr3[] = {6580,8822,1968,673,1394,9337,5486,1746,5229,4092,195}; vector <int> Arg3(Arr3, Arr3 + (sizeof(Arr3) / sizeof(Arr3[0]))); long long Arg4 = 937186357646035002LL; string Arg5 = "Impossible"; verify_case(5, Arg5, isAble(Arg0, Arg1, Arg2, Arg3, Arg4)); }
 
-// END CUT HERE
+	// END CUT HERE
 
 };
 
 // BEGIN CUT HERE
 int main()
 {
-LongLongTripDiv1 ___test;
-___test.run_test(-1);
-system("pause");
+	LongLongTripDiv1 ___test;
+	___test.run_test(-1);
 }
 // END CUT HERE
