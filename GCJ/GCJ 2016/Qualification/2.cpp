3d3f3ff6b08f92e44ad05c7083d7a21c5581b050
Sosi <sosisarah@gmail.com>
2016-07-09 19:05:32 +0800

    test、

---
+++ GCJ/GCJ 2016/Qualification/2.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+template<class T> T gcd(T a,T b)
+{
+    if(a<0) return gcd(-a,b);
+    if(b<0)return gcd(a,-b);
+    return (b==0)?a:gcd(b,a%b);
+}
+template <class T> T lcm(T a,T b)
+{
+    return a*(b/gcd(a,b));
+}
+template<class T> inline vector<pair<T,int> > factorize(T n)
+{
+    vector<pair<T,int> > R;
+    for (T i=2; n>1;)
+    {
+        if (n%i==0)
+        {
+            int C=0;
+            for (; n%i==0; C++,n/=i);
+            R.push_back(make_pair(i,C));
+        }
+        i++;
+        if (i>n/i) i=n;
+    }
+    if (n>1) R.push_back(make_pair(n,1));
+    return R;
+}
+template<class T> inline bool isPrimeNumber(T n)
+{
+    if(n<=1)return false;
+    for (T i=2; i*i<=n; i++) if (n%i==0) return false;
+    return true;
+}
+template<class T> inline T eularFunction(T n)
+{
+    vector<pair<T,int> > R=factorize(n);
+    T r=n;
+    for (int i=0; i<R.size(); i++)r=r/R[i].first*(R[i].first-1);
+    return r;
+}
+template<class T> string toString(T n)
+{
+    ostringstream ost;
+    ost<<n;
+    ost.flush();
+    return ost.str();
+}
+int toInt(string s)
+{
+    int r=0;
+    istringstream sin(s);
+    sin>>r;
+    return r;
+}
+
+int i,j,k;
+#define REP(i,n) for((i)=0;(i)<(int)(n);(i)++)
+#define snuke(c,itr) for(__typeof((c).begin()) itr=(c).begin();itr!=(c).end();itr++)
+int dirx[]= {1, 1, 0,-1,-1,-1,0,1};
+int diry[]= {0,-1,-1,-1, 0, 1,1,1}; //clockwise
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+//    freopen("2.txt","r",stdin);
+//    freopen("B-small-practice.in","r",stdin);
+//    freopen("B-small-practice.out","w",stdout);
+    freopen("B-large-practice.in","r",stdin);
+    freopen("B-large-practice.out","w",stdout);
+    int T;
+    cin>>T;
+    for(int cas = 1; cas<= T; cas++)
+    {
+        string s; cin>>s;
+        int ret = 0;
+        for(int i = s.size()-1; i>=0; i--)
+        {
+            if(ret%2==0 && s[i] == '-' ||
+               ret%2==1 && s[i] == '+')
+            {
+                ret++;
+            }
+        }
+        cout<<"Case #"<<cas<<": "<<ret<<endl;
+    }
+    return 0;
+}
